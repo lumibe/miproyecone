9e964b34e842e8ca3b5089d7780e3caab8a0c21f
Luis Miguel <lumibe@gmail.com>
2013-08-09 11:08:09 -0500

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miproyecone::Application.config.secret_token = 'fde0a23c3240973832fe23b4bd6bdc2eeaa9b3e371dac6ec02bd3fab0fdbaca81471ba08e5157cb9b767512230cdf073609ca8a1fb4e2efe4e964fc46d310f10'
